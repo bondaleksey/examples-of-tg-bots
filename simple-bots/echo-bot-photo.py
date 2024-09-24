37f442fec80a17b4bef717c609267c99a4a84a99
Aleksey Bondarenko <bondaleksey@gmail.com>
2024-09-24 16:17:18 +0300

    simple echo bots

--- simple-bots/echo-bot-photo.py
+++ simple-bots/echo-bot-photo.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from dotenv import dotenv_values
+from aiogram import F
 
 
 ENV = dotenv_values(".env")
@@ -14,13 +15,11 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer("Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь")
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer(
         "Напиши мне что-нибудь и в ответ " "я пришлю тебе твое сообщение"
@@ -29,10 +28,20 @@ async def process_help_command(message: Message):
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands="start"))
+dp.message.register(process_help_command, Command(commands="help"))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_echo)
 
 if __name__ == "__main__":
     dp.run_polling(bot)
--- simple-bots/full_echo_bot.py
+++ simple-bots/full_echo_bot.py
@@ -1,12 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
-from dotenv import dotenv_values
 
-
-ENV = dotenv_values(".env")
-BOT_TOKEN = str(ENV["RUTUBE_HELPER_BOT_TOKEN"])
-
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+BOT_TOKEN = "BOT TOKEN HERE"
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
@@ -14,24 +12,29 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
+@dp.message(Command(commands="start"))
 async def process_start_command(message: Message):
     await message.answer("Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь")
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=["help"]))
+@dp.message(Command(commands="help"))
 async def process_help_command(message: Message):
     await message.answer(
         "Напиши мне что-нибудь и в ответ " "я пришлю тебе твое сообщение"
     )
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text="Данный тип апдейтов не поддерживается " "методом send_copy"
+        )
 
 
 if __name__ == "__main__":
